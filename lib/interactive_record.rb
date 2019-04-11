308a083c824f8ea27977145ac9305e94627bda05
Sachin Mahajan <sachinm78@gmail.com>
2019-04-11 20:06:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,31 +4,36 @@ require 'active_support/inflector'
 class InteractiveRecord
   
   def self.table_name
-      self.to_s.downcase.pluralize
+    self.to_s.downcase.pluralize
   end
 
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
+    table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+
+    table_columns.each do |col|
+      column_names << col["name"]
     end
+
     column_names.compact
   end
 
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
+  def initialize(objects={})
+    objects.each do |k, v|
+      self.send("#{k}=", v)
     end
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert}(#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+      SQL
+
     DB[:conn].execute(sql)
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
@@ -36,21 +41,26 @@ class InteractiveRecord
     self.class.table_name
   end
 
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
   def values_for_insert
     values = []
+
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-    DB[:conn].execute(sql, name)
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE name = ?
+      SQL
+
+    DB[:conn].execute(sql, name);
   end
   
   
